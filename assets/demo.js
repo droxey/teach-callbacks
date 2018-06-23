630ee26cf360a9d6217aa81e1aaa196077ec2fa4
Dani Roxberry <dani@bitoriented.com>
2018-06-23 16:43:43 -0700

    [add] connectDevice, writeLine(cmd) for demo

--- assets/demo.js
+++ assets/demo.js
@@ -1,12 +1,49 @@
-var typed = new Typed("#typed", {
-  stringsElement: "#typed-strings",
-  typeSpeed: 80,
-  smartBackspace: true
-});
+var connection;
 
-// Why isn't this a normal function call that returns a value?
-// It takes time to send the data to Puck.js and get a response back!
-// If your code waited for a response then the whole webpage would grind to a halt.
-Puck.eval("BTN.read()", function(x) {
-  console.log(x);
-});
+function connectDevice() {
+  if (connection) {
+    connection.close();
+    connection = undefined;
+  }
+
+  var readLine = function(line) {
+    try {
+      var j = JSON.parse(line);
+      console.log("Received JSON: ", j);
+    } catch (e) {
+      console.log("Received: ", line);
+    }
+  };
+
+  Puck.connect(function(c) {
+    if (!c) {
+      console.error("Couldn't connect --- did you cancel the pairing dialog?");
+      return;
+    }
+
+    connection = c;
+    var buf = "";
+    connection.on("data", function(d) {
+      buf += d;
+      var i = buf.indexOf("\n");
+      while (i >= 0) {
+        readLine(buf.substr(0, i));
+        buf = buf.substr(i + 1);
+        i = buf.indexOf("\n");
+      }
+    });
+  });
+}
+
+function writeLine(cmd) {
+  connection.write("reset();\n", function() {
+    setTimeout(function() {
+      connection.write(
+        `setInterval(function(){Bluetooth.println(${cmd});},500);NRF.on('disconnect', function() {reset()});\n`,
+        function() {
+          console.log("Ready...");
+        }
+      );
+    }, 1500);
+  });
+}
