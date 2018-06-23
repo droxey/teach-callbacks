ac137dc39bd82ad544858090a30b88983b6198c1
Dani Roxberry <dani@bitoriented.com>
2018-06-23 05:06:06 -0700

    [add] code viz ux

--- assets/demo.js
+++ assets/demo.js
@@ -1,3 +1,9 @@
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  typeSpeed: 80,
+  smartBackspace: true
+});
+
 // Why isn't this a normal function call that returns a value?
 // It takes time to send the data to Puck.js and get a response back!
 // If your code waited for a response then the whole webpage would grind to a halt.
